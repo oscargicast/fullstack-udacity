5610d1d00afaef6af776a81ab042d5867602a8fc
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-07-13 03:03:23 -0500

    expose a naive api

--- src/runserver.py
+++ src/runserver.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, MenuItem
@@ -9,6 +10,23 @@ app = Flask(__name__)
 engine = create_engine('sqlite:///restaurantmenu.db')
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+# Making an API Endpoint (Get Request).
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON/')
+def RestaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/JSON/')
+def MenuItemJSON(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id,
+        id=menu_id,
+    ).one()
+    return jsonify(MenuItems=item.serialize)
 
 
 @app.route('/')
